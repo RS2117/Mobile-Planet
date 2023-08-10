edfe5804093d6876ab3d9e8ff7442e286e9c8a68
RS2117 <34489935+RS2117@users.noreply.github.com>
2023-08-10 10:25:40 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const mongoDB_Url = process.env.mongoDB_Url;
 
+mongoose.connect(config.DB,{ useMongoClient:true });
 mongoose.connect(mongoDB_Url);
 mongoose.connection.on('connected',()=>{
     console.log('connected');
