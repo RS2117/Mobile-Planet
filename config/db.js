52abe919944f383732a6fa6a5754a32ea398ced5
RS2117 <34489935+RS2117@users.noreply.github.com>
2023-08-10 10:32:07 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const mongoDB_Url = process.env.mongoDB_Url;
 
-await mongoose.connect(mongoDB_Url);
+mongoose.connect(mongoDB_Url);
 mongoose.connection.on('connected',()=>{
     console.log('connected');
 });
